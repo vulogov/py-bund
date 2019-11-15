fec01d6043e583c9ccfb194ec732cbf4b9d4d7e1
Vladimir Ulogov <vulogov@linkedin.com>
2019-11-15 10:32:52 -0800

    Intital submission

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+setup(name='bund',
+      version='0.1',
+      description='BUND language interpreter library',
+      url='https://github.com/vulogov/py-bund',
+      author='Vladimir Ulogov',
+      author_email='vulogov@linkedin.com',
+      license='GPL3',
+      packages=['bund'],
+      zip_safe=False)
