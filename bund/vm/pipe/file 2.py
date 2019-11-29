886bf22cd5ce4770e89bbf9f2a0b100b1580f878
Vladimir Ulogov <vladimir.ulogov@me.com>
2019-11-29 03:53:18 -0800

    Remove duplicates

--- bund/vm/pipe/file 2.py
+++
@@ -1,8 +0,0 @@
-def init(namespace, **kw):
-    return True
-
-def read(namespace, name):
-    return None
-
-def write(namespace, name, data):
-    return None
--- bund/vm/runtime_vars 2.py
+++
@@ -1,7 +0,0 @@
-from bund.library.ns import *
-
-def vmRtGet(namespace, name, default=None):
-    return nsGet(namespace, "/sys/runtime/{}".format(name), default)
-
-def vmRtSet(namespace, name, value):
-    return nsSet(namespace, "/sys/runtime/{}".format(name), value)
